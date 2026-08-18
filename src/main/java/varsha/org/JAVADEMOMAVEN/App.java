c13d8ca4b6e0fcb0f90179bc71841043669f4ccf
Varsha <jvarsha200702@gmail.com>
2026-08-18 11:15:16 +0530

    First Commit

---
+++ src/main/java/varsha/org/JAVADEMOMAVEN/App.java
@@ -0,0 +1,10 @@
+package varsha.org.JAVADEMOMAVEN;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Folks ,welcome to KMIT");
+    }
+}
